232ee1caf41f413a00a787f7e286d43872127a62
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2025-10-11 20:55:57 +0300

    Minor fix: Postgres configuration

--- LabsQueueBot.Core/Settings/PostgreSqlSettings.cs
+++ LabsQueueBot.Core/Settings/PostgreSqlSettings.cs
@@ -2,5 +2,9 @@
 
 public class PostgreSqlSettings
 {
-    public required string ConnectionString { get; init; }
+    public required string Host { get; init; }
+    public required string Port { get; init; }
+    public required string Database { get; init; }
+    public required string Username { get; init; }
+    public required string Password { get; init; }
 }
--- LabsQueueBot.Web/ServiceCollectionExtensions/DbContextConfigurator.cs
+++ LabsQueueBot.Web/ServiceCollectionExtensions/DbContextConfigurator.cs
@@ -6,12 +6,15 @@ namespace LabsQueueBot.Web.ServiceCollectionExtensions;
 
 public static class DbContextConfigurator
 {
+    private const string ConnectionStringPattern = "Host={0};Port={1};Database={2};Username={3};Password={4}";
+
     public static IServiceCollection AddDbContext(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
         var settings = configuration.GetRequiredSection(nameof(PostgreSqlSettings)).Get<PostgreSqlSettings>();
         ArgumentNullException.ThrowIfNull(settings);
-
-        var connectionString = settings.ConnectionString;
+        
+        var connectionString = string.Format(ConnectionStringPattern,
+            settings.Host, settings.Port, settings.Database, settings.Username, settings.Password);
         serviceCollection.AddDbContextFactory<QueueBotContext>(
             options => { options.UseNpgsql(connectionString); },
             ServiceLifetime.Scoped);
